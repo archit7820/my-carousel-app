be1f8e90023300eddaaf51ec538f54943b73c608
Archit <archit.mymail@gmail.com>
2023-11-24 15:31:56 +0530

    Initial commit

---
+++ src/slider-data.js
@@ -0,0 +1,22 @@
+import slide1Image from './assets/slide1.jpg';
+import slide2Image from './assets/slide2.jpg';
+import slide3Image from './assets/slide3.jpg';
+
+
+export const sliderData = [
+  {
+    image: slide1Image , 
+    heading: "Talent Acquisition ,Hiring Services",
+    desc: "Train to Hire | Campus Hiring | Conducting Induction Training ",
+  },
+  {
+    image: slide2Image,
+    heading: "Coding For Kids Program" ,
+    desc: "Empower your child with gift of coding" ,
+  },
+  {
+    image: slide3Image,
+    heading: "customised Training",
+    desc: "Bringing success by Skilling-up to the organizations and its professionals",
+  },
+];
